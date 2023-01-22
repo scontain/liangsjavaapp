23cea6764382c3d4ca474fd766f2ad68330afe37
Christof Fetzer <christof.fetzer@scontain.com>
2023-01-23 00:09:00 +0100

    using extended run script

--- check_prerequisites.sh
+++ check_prerequisites.sh
@@ -48,16 +48,16 @@ then
 fi
 
 echo -e "${BLUE}Checking that we run applications with docker without sudo${NC}"
-if ! docker run -it --rm hello-world &> /dev/null
+if ! docker run --rm hello-world &> /dev/null
 then
     echo -e "${RED}Docker does not seem to run."
     echo -e "Please ensure that you can run docker without sudo: https://docs.docker.com/engine/install/linux-postinstall/." 
-    echo -e "Ensure that command 'docker run -it hello-world' runs without problems${NC}"
+    echo -e "Ensure that command 'docker run hello-world' runs without problems${NC}"
     error_exit
 fi
 
 echo -e "${BLUE}Checking that we can run container images for linux/amd64${NC}"
-if ! docker run --platform linux/amd64 -it --rm hello-world &> /dev/null
+if ! docker run --platform linux/amd64 --rm hello-world &> /dev/null
 then
     echo -e "${RED}Docker does not seem to support argument '--platform linux/amd64'"
     echo -e "Please ensure that you can run the latest version of docker (i.e.,  API version >= 1.40)" 
@@ -73,7 +73,7 @@ then
 fi
 
 echo -e "${BLUE}Checking that you can pull the images ${NC}"
-if docker pull --platform linux/amd64 -it registry.scontain.com/sconectl/check_cpufeatures:latest &> /dev/null
+if ! docker pull --platform linux/amd64 registry.scontain.com/sconectl/check_cpufeatures:latest &> /dev/null
 then
     echo -e "${RED}Docker does NOT seem to be able to pull the required container images.${NC}"
     echo -e "- ${ORANGE}1. Register an account with your company email at https://gitlab.scontain.com/users/sign_up.${NC}"
@@ -84,11 +84,11 @@ then
 fi
 
 echo -e "${BLUE}Checking that we the CPU has all necessary CPU features enabled${NC}"
-if ! docker run --platform linux/amd64 -it --rm registry.scontain.com/sconectl/check_cpufeatures:latest &> /dev/null
+if ! docker run --platform linux/amd64 --rm registry.scontain.com/sconectl/check_cpufeatures:latest &> /dev/null
 then
     echo -e "${RED}Docker does not seem to support all CPU features.${NC}"
     echo -e "- ${ORANGE}Assuming you do not run on a modern Intel CPU. Please ensure that you pass the following options to qemu: -cpu qemu64,+ssse3,+sse3,+sse4.1,+sse4.2,+rdrand,+popcnt,+xsave,+aes${NC}" 
-    error_exit
+    warning "Sconfication will most likely fail! Please run in an Virtual Machine."
 fi
 
 echo -e "${BLUE}Checking that we have access to kubectl${NC}"
@@ -132,15 +132,44 @@ if ! kubectl get pods &> /dev/null
 then
     echo -e "${RED}It seems that you do not have access to a Kubernetes cluster!${NC}"
     echo -e "- ${ORANGE}Please ensure that you have access to a Kubernetes cluster${NC}"
+    error_exit
 fi
 
 
 echo -e "${BLUE}Checking that you have the local attestation service, the SGX Plugin, and the image pull secrets installed${NC}"
 #if ! sconectl scone_init &> /dev/null
-if ! ((kubectl get las | grep HEALTHY) && (kubectl get cas | grep HEALTHY) && (kubectl get sgxplugin | grep HEALTHY))
+if ! ((kubectl get las | grep HEALTHY) && (kubectl get sgxplugin | grep HEALTHY))
 then
     echo -e "${RED}It seems the Kubernetes cluster is not yet properly initialized!${NC}"
     echo -e "- ${ORANGE}1. Retrieve/create an access token https://sconedocs.github.io/registry/#create-an-access-token${NC}"
     echo -e "- ${ORANGE}2. Install the SCONE operator: https://sconedocs.github.io/2_operator_installation/"
     echo -e "- ${ORANGE}3. Install SGXPlugin, LAS, and CAS: https://sconedocs.github.io/4_quickstart/${NC}"
+    error_exit
 fi
+
+echo -e "${BLUE}Checking if the CAS '$CAS' in namespace '$CAS_NAMESPACE' is installed${NC}"
+if ! (kubectl get cas "$CAS" -n "$CAS_NAMESPACE" )
+then
+    echo -e "${RED}It seems that CAS '$CAS' in namespace '$CAS_NAMESPACE' is not yet running!${NC}"
+    error_exit
+fi
+
+STATUS=$(kubectl get cas "$CAS" -n "$CAS_NAMESPACE"  --output=json | jq '.status.state' || echo "CAS NOT YET RUNNING")
+echo -e "${BLUE}Checking health status of CAS '$CAS' in namespace '$CAS_NAMESPACE' is installed${NC}"
+if ! [[ "$STATUS" == "\"HEALTHY\"" ]]
+then
+    echo -e "${RED}It seems that CAS '$CAS' in namespace '$CAS_NAMESPACE' is not healthy: status is $STATUS${NC}"
+    echo -e "- ${ORANGE}- You can install cas as follows: kubectl provision cas $CAS $CAS_NAMESPACE -v${NC}"
+    error_exit
+fi
+
+echo -e "${BLUE}Checking health status of CAS '$CAS' in namespace '$CAS_NAMESPACE' is provisioned${NC}"
+
+if ! kubectl provision cas "$CAS" -n "$CAS_NAMESPACE" --is-provisioned
+then
+    echo -e "${RED}It seems that CAS '$CAS' in namespace '$CAS_NAMESPACE' is not yet provisioned${NC}"
+    echo -e "- ${ORANGE}- You can provision this cas as follows: kubectl provision cas $CAS $CAS_NAMESPACE -v${NC}"
+    error_exit
+fi
+
+
