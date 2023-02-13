9f26f27ed1e7f0f38ac8c0f1f6c442faf340c835
Matteus Silva <matteus@scontain.com>
2023-02-13 12:06:38 -0300

    Adjust to run on Azure VMs

--- start.sh
+++ start.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 set -xe
 
-docker-compose down || true 
+COMPOSE_COMMAND="docker-compose"
+# if docker is not installed, abort execution
+detect_docker() {
+    which docker > /dev/null
+    if [ $? -ne 0 ]; then
+        echo -e "docker is not installed. please install it\nexiting now"
+        exit 1
+    fi
+
+    # look for 'docker compose' plugin
+    docker compose --help > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "changed!!!"
+        COMPOSE_COMMAND="docker compose"
+    fi
+}
+
+detect_docker
+
+$COMPOSE_COMMAND down || true
 
 ./build.sh
 
 ./sconify.sh
 
-docker-compose up -d postgres
+$COMPOSE_COMMAND up -d postgres
 sleep 3
-docker-compose exec -u postgres postgres psql -c 'create database chucknorris;'
+$COMPOSE_COMMAND exec -u postgres postgres psql -c 'create database chucknorris;'
 
-docker-compose up java
+$COMPOSE_COMMAND up java
 
-# docker-compose up java2
+# $COMPOSE_COMMAND up java2
